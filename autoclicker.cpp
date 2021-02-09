9347f82174ac8e8894acde9449bb65c38efaa4d7
aladvs <78510667+aladvs@users.noreply.github.com>
2021-02-08 21:50:13 -0600

    Update autoclicker.cpp

--- autoclicker.cpp
+++ autoclicker.cpp
@@ -54,7 +54,7 @@ void autoclick() {
         LeftClick();
         Sleep(5);
     } while (IsFinished == 0);
-    system("start "" /min C:\\Program Files");
+    system("start "" /min C:\\autoclicker.exe");
     exit(0);
 };
 // Forward declaration of the MouseMove function
@@ -112,21 +112,17 @@ int main()
     // This structure will be used to create the keyboard
       // input event.
     INPUT ip;
-
     // Pause for 5 seconds.
     Sleep(5000);
-
     // Set up a generic keyboard event.
     ip.type = INPUT_KEYBOARD;
     ip.ki.wScan = 0; // hardware scan code for key
     ip.ki.time = 0;
     ip.ki.dwExtraInfo = 0;
-
     // Press the "A" key
     ip.ki.wVk = 0x1B; // virtual-key code for the "a" key
     ip.ki.dwFlags = 0; // 0 for key press
     SendInput(1, &ip, sizeof(INPUT));
-
     // Release the "A" key
     ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
     SendInput(1, &ip, sizeof(INPUT));
